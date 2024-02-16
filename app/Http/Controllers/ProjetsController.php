9cdfa85c7c471410bed8f19fe1cf7bac55ec0494
MKSOW <134109217+MKSOW@users.noreply.github.com>
2024-02-16 21:05:33 +0100

    Add files via upload

---
+++ app/Http/Controllers/ProjetsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Projet;
+
+class ProjetsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $Projets=Projet::all();
+        return view('Projets.index',['Projet'=>$Projets]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('Projets.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required|min:3|max:255',
+            'description' => 'required|min:3|max:255',
+            'debut_projet' => 'required|min:3|max:255',
+            'fin_projet' => 'required|min:3|max:255',
+            'listeTaches' => 'required|min:3|max:255'
+
+        ]);
+        Projet::create($request->post());
+        return redirect()->route('Projets.index')->with('status','l\'entreprise a été ajouter avec succés');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Projet $Projet)
+    {
+        return view('Projets.show', ['Projet' => $Projet]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Projet $Projet)
+    {
+        return view('Projets.edit', ['Projet' => $Projet]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Projet $Projet)
+    {
+        $request->validate([
+            'nom' => 'required|min:3|max:255',
+            'description' => 'required|min:3|max:255',
+            'debut_projet' => 'required|min:3|max:255',
+            'fin_projet' => 'required|min:3|max:255',
+            'listeTaches' => 'required|min:3|max:255'
+
+        ]);
+        $Projet->update($request->post());
+        return redirect()->route('Projets.index')->with('status','Le nom du Projet à été ajouter avec succès');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Projet $Projet)
+    {
+        $Projet->delete();
+        return redirect()->route('Projets.index');
+    }
+}
